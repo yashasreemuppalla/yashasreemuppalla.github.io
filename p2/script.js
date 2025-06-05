818b2c14ad9514af7a02547f3f44f478385d3db9
yashasreemuppalla <yashasreemuppalla@gmail.com>
2025-06-05 19:42:05 +0530

    modified:   p2/script.js 	modified:   p2/styles.css

--- p2/script.js
+++ p2/script.js
@@ -4,7 +4,7 @@ const showLogin = () => {
     <h1>Login Form</h1>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
+    <p><button onclick='showWelcome()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
@@ -13,12 +13,23 @@ const showLogin = () => {
 
 const showRegister = () => {
       let str=`
+    <div class='App-Container'>
     <h1>Register Form</h1>
     <p><input type="text" id="txtName"></p>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
     <button>Register</button>
     <p><a href ="#" onclick='showLogin()'>Already have an account?</a></p>
+    `
+    root.innerHTML = str
+}
+
+const showWelcome = () => {
+    let str=`
+    <div class='App-Container'>
+    <h1>Welcome</h1>
+    <p><button onclick='showLogin()'>Log Out</button></p>
+    </div>
     `
     root.innerHTML = str
 }
